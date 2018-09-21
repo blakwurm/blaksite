dc4b8f69d3288407b5ffe6c2388e6c1dc2843ae0
Alexander Chythlook <achythlook@gmail.com>
2018-09-21 11:59:55 -0800

    Add a cmd flag

--- blaksite.py
+++ blaksite.py
@@ -2,9 +2,12 @@
 from html.parser import HTMLParser
 from json import dumps, loads
 
-def main():
+def getSiteOpts(siteopts_location):
+    return loads(open(siteopts_location).read())
+
+def main(siteopts: ("Site Config File" "option" "s") = "siteopts.json"):
     print("This is hello world!")
-    print(loads(open("siteopts.json").read())['pages'][1])
+    print(getSiteOpts(siteopts)['pages'][1])
 
 if __name__ == '__main__':
     import plac; plac.call(main)
