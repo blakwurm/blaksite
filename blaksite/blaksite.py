0d990942f71fb63069c5d77e49ca218236d0fe32
Alexander Chythlook <achythlook@gmail.com>
2018-09-26 10:15:07 -0800

    Remote redundant functions from blaksite

--- blaksite/blaksite.py
+++ blaksite/blaksite.py
@@ -7,69 +7,6 @@ from wurmforge import WurmForge
 
 __parser = 'html5lib'
 
-def getSiteSettings(siteopts_location):
-    sitesettings = loads(open(siteopts_location).read())
-    return sitesettings 
-
-def grabHTML(filename):
-    return BeautifulSoup(open("template/" + filename), __parser)
-
-def changeInnerElement(element, htmlstring):
-    #if (element != None):
-    #element.clear()
-    #element.append(BeautifulSoup(htmlstring, __parser))
-    print("htmlstring is " + htmlstring)
-    print("element is ")
-    print(element)
-    element.string.replace_with(BeautifulSoup(htmlstring, __parser))
-    return element
-
-def passtest(arg):
-    return arg
-
-def makenavlist(siteopts):
-    blankbody = BeautifulSoup('', __parser)
-    ul = blankbody.new_tag(name ='ul')
-    for page, pagedata in siteopts['pages'].items():
-        li = blankbody.new_tag(name = 'li')
-        a = blankbody.new_tag(name = 'a', href = pagedata['url'])
-        a.append(pagedata['title'])
-        li.append(a)
-        ul.append(li)
-    return ul
-
-
-def makeMainPage(siteopts):
-    page = grabHTML("index.html")
-    print(page.select_one('title'))
-    print(page.select_one('.sitetitle'))
-    print(page.select_one('.navbar'))
-    page.head.title.string = siteopts['title']
-    page.find(class_ = 'sitetitle').string = siteopts['name']
-    page.find(class_ = 'sitetagline').string = siteopts['tagline']
-    #passtest(page.select_one('.navbar')).string = "<li>doodad</li>"
-    navbar = page.find(class_ = 'navbar')
-    navbar.clear()
-    navbar.append(makenavlist(siteopts))
-    #changeInnerElement(page.find(class_ = "navbar"), '<li>doodad</li>'), 
-    #changeInnerElement(page.select_one('title'), siteopts['title'])
-    #changeInnerElement(page.select_one('.sitetitle'), siteopts['title'])
-   # page.find(class_ = "sitetitle").string.replaceWith(siteopts['name'])
-    return page
-
-def setupOutput(siteopts):
-    try:
-        shutil.rmtree(siteopts['output'])
-    except:
-        print("No output folder just yet")
-    shutil.copytree("template", siteopts['output'])
-    return siteopts
-    
-def writePage(siteopts, filename, page):
-    openfile = open(siteopts['output'] + "/" + filename, "w")
-    openfile.write(page)
-    openfile.close
-
 def main(siteopts = "sitesettings.json"):
     forge = WurmForge(siteopts)
     forge.makeSite()
@@ -78,7 +15,6 @@ def main(siteopts = "sitesettings.json"):
     print(forge.makeNavList('Home'))
     print(forge.makePages())
     print("page names")
-    print(makenavlist(getSiteSettings(siteopts)))
 
 if __name__ == '__main__':
     import plac; plac.call(main)
