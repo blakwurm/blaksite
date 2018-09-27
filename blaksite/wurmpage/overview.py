2cafca0d4bea68f4209404bca2c1f1faf7dcbf54
Alex Chythlook <achythlook@gmail.com>
2018-09-27 07:51:40 -0800

    Forgot to merge from origin...

--- blaksite/wurmpage/overview.py
+++ blaksite/wurmpage/overview.py
@@ -3,4 +3,7 @@
 
 def makeOverviewPage(forge, pagekey):
     pagedef, soup, navlist = forge.makeStarterKit(pagekey)
+    select = soup.select_one
+    select('title').string = forge.sitesettings['title']
+    select('.sitetitle').string = forge.sitesettings['name']
     return {'overviiiiii': str(soup)}
